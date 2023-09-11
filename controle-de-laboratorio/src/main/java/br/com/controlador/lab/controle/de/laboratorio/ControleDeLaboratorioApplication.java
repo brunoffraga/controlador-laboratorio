91a12ff5ce441fc862746b90c7ddbb31d4b99ae1
Bruno Fraga <brunofraga19@gmail.com>
2023-09-11 16:46:26 -0300

    inicio do projeto

---
+++ controle-de-laboratorio/src/main/java/br/com/controlador/lab/controle/de/laboratorio/ControleDeLaboratorioApplication.java
@@ -0,0 +1,13 @@
+package br.com.controlador.lab.controle.de.laboratorio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleDeLaboratorioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleDeLaboratorioApplication.class, args);
+	}
+
+}
